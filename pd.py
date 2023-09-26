36a30abae0a6723b75dbd69023195313e6ab92cf
Marcus Bannerman <m.bannerman@gmail.com>
2023-09-27 00:14:03 +0100

    Regsisters are parsed

--- pd.py
+++ pd.py
@@ -22,6 +22,84 @@ import re
 import sigrokdecode as srd
 from common.srdhelper import bcd2int, SrdIntEnum
 
+REGs = {
+ 0x06: ('BCD_TYPEC_REV_LOW', None),
+ 0x07: ('BCD_TYPEC_REV_HIGH', None),
+ 0x08: ('BCD_USBPD_REV_LOW', None),
+ 0x09: ('BCD_USBPD_REV_HIGH', None),
+ 0x0A: ('DEVICE_CAPAB_HIGH', None),
+ 0x0B: ('ALERT_STATUS_1', None),
+ 0x0C: ('ALERT_STATUS_1_MASK', None),
+ 0x0D: ('PORT_STATUS_0', None),
+ 0x0E: ('PORT_STATUS_1', None),
+ 0x0F: ('TYPEC_MONITORING_STATUS_0', None),
+ 0x10: ('TYPEC_MONITORING_STATUS_1', None),
+ 0x11: ('CC_STATUS', None),
+ 0x12: ('CC_HW_FAULT_STATUS_0', None),
+ 0x13: ('CC_HW_FAULT_STATUS_1', None),
+ 0x14: ('PD_TYPEC_STATUS', None),
+ 0x15: ('TYPEC_STATUS', None),
+ 0x16: ('PRT_STATUS', None),
+ 0x1A: ('PD_COMMAND_CTRL', None),
+ 0x20: ('MONITORING_CTRL_0', None),
+ 0x22: ('MONITORING_CTRL_2', None),
+ 0x23: ('RESET_CTRL', None),
+ 0x25: ('VBUS_DISCHARGE_TIME_CTRL', None),
+ 0x26: ('VBUS_DISCHARGE_CTRL', None),
+ 0x27: ('VBUS_CTRL', None),
+ 0x29: ('PE_FSM', None),
+ 0x2D: ('GPIO_SW_GPIO', None),
+ 0x2F: ('Device_ID', None),
+ 0x31: ('RX_HEADER_LOW', None),
+ 0x32: ('RX_HEADER_HIGH', None),
+ 0x33: ('RX_DATA_OBJ1_0', None),
+ 0x34: ('RX_DATA_OBJ1_1', None),
+ 0x35: ('RX_DATA_OBJ1_2', None),
+ 0x36: ('RX_DATA_OBJ1_3', None),
+ 0x37: ('RX_DATA_OBJ2_0', None),
+ 0x38: ('RX_DATA_OBJ2_1', None),
+ 0x39: ('RX_DATA_OBJ2_2', None),
+ 0x3A: ('RX_DATA_OBJ2_3', None),
+ 0x3B: ('RX_DATA_OBJ3_0', None),
+ 0x3C: ('RX_DATA_OBJ3_1', None),
+ 0x3D: ('RX_DATA_OBJ3_2', None),
+ 0x3E: ('RX_DATA_OBJ3_3', None),
+ 0x3F: ('RX_DATA_OBJ4_0', None),
+ 0x40: ('RX_DATA_OBJ4_1', None),
+ 0x41: ('RX_DATA_OBJ4_2', None),
+ 0x42: ('RX_DATA_OBJ4_3', None),
+ 0x43: ('RX_DATA_OBJ5_0', None),
+ 0x44: ('RX_DATA_OBJ5_1', None),
+ 0x45: ('RX_DATA_OBJ5_2', None),
+ 0x46: ('RX_DATA_OBJ5_3', None),
+ 0x47: ('RX_DATA_OBJ6_0', None),
+ 0x48: ('RX_DATA_OBJ6_1', None),
+ 0x49: ('RX_DATA_OBJ6_2', None),
+ 0x4A: ('RX_DATA_OBJ6_3', None),
+ 0x4B: ('RX_DATA_OBJ7_0', None),
+ 0x4C: ('RX_DATA_OBJ7_1', None),
+ 0x4D: ('RX_DATA_OBJ7_2', None),
+ 0x4E: ('RX_DATA_OBJ7_3', None),
+ 0x51: ('TX_HEADER_LOW', None),
+ 0x52: ('TX_HEADER_HIGH', None),
+ 0x70: ('DPM_PDO_NUMB', None),
+ 0x85: ('DPM_SNK_PDO1_0', None),
+ 0x86: ('DPM_SNK_PDO1_1', None),
+ 0x87: ('DPM_SNK_PDO1_2', None),
+ 0x88: ('DPM_SNK_PDO1_3', None),
+ 0x89: ('DPM_SNK_PDO2_0', None),
+ 0x8A: ('DPM_SNK_PDO2_1', None),
+ 0x8B: ('DPM_SNK_PDO2_2', None),
+ 0x8C: ('DPM_SNK_PDO2_3', None),
+ 0x8D: ('DPM_SNK_PDO3_0', None),
+ 0x8E: ('DPM_SNK_PDO3_1', None),
+ 0x8F: ('DPM_SNK_PDO3_2', None),
+ 0x90: ('DPM_SNK_PDO3_3', None),
+ 0x91: ('RDO_REG_STATUS_0', None),
+ 0x92: ('RDO_REG_STATUS_1', None),
+ 0x93: ('RDO_REG_STATUS_2', None),
+ 0x94: ('RDO_REG_STATUS_3', None),
+}
 
 class Decoder(srd.Decoder):
     api_version = 3
@@ -73,27 +151,30 @@ class Decoder(srd.Decoder):
 
     def putd(self, bit1, bit2, data):
         self.put(self.bits[bit1][1], self.bits[bit2][2], self.out_ann, data)
+    
     #def putr(self, bit):
     #    self.put(self.bits[bit][1], self.bits[bit][2], self.out_ann,
     #             [Ann.BIT_RESERVED, ['Reserved bit', 'Reserved', 'Rsvd', 'R']])
 
     def handle_write_reg(self, b):
-        self.putd(7, 0, [self.ANN_REGISTER, ['Write Reg %02X'%self.reg]])
-
+        if self.reg in REGs:
+            name = REGs[self.reg][0]
+            self.putx([self.ANN_REGISTER, ['Write '+name, name]])
+        else:
+            self.putx([self.ANN_REGISTER, ['Write Unknown reg %02X'%self.reg, "Unknown"]])
         #fn = getattr(self, 'handle_reg_0x%02x' % r)
         #fn(b)
-        # Honor address auto-increment feature of the DS1307. When the
-        # address reaches 0x3f, it will wrap around to address 0.
         self.reg += 1
-        #if self.reg > 0x3f:
-        #    self.reg = 0
 
     def handle_read_reg(self, b):
-        self.putd(7, 0, [self.ANN_REGISTER, ['Read Reg %02X'%self.reg]])
+        if self.reg in REGs:
+            name = REGs[self.reg][0]
+            self.putx([self.ANN_REGISTER, ['Read '+name, name]])
+        else:
+            self.putx([self.ANN_REGISTER, ['Read Unknown reg %02X'%self.reg, "Unknown"]])
         #fn = getattr(self, 'handle_reg_0x%02x' % r)
         #fn(b)
         self.reg += 1
-
 
     def handle_ACK(self):
         if self.needACK == True:
@@ -103,7 +184,6 @@ class Decoder(srd.Decoder):
         else:
             self.putx([self.ANN_ERROR, ['Unxpected ACK']])
             return False
-
 
     def is_correct_chip(self, addr):
         addr = addr >> 1
@@ -116,7 +196,11 @@ class Decoder(srd.Decoder):
 
     def setReg(self, reg):
         self.reg = reg
-        self.putx([self.ANN_REGISTER, ['Reg %02X' % self.reg]])
+        if self.reg in REGs:
+            name = REGs[self.reg][0]
+            self.putx([self.ANN_REGISTER, ["Register "+name, name]])
+        else:
+            self.putx([self.ANN_REGISTER, ["Unknown Register %02X"%self.reg]])
 
     def decode(self, ss, es, data):
         cmd, databyte = data
@@ -176,7 +260,6 @@ class Decoder(srd.Decoder):
             self.state = 'WRITE REGS'
             self.needACK = True
         elif self.state == 'WRITE REGS':
-
             if cmd == "ACK":
                 if self.handle_ACK():
                     return
@@ -191,9 +274,7 @@ class Decoder(srd.Decoder):
                 self.state = 'IDLE'
             else:
                 self.putx([self.ANN_ERROR, ['Expected DATA WRITE or STOP', 'ERR']])
-
         elif self.state == 'READ REGS':
-
             if cmd == "ACK":
                 if self.handle_ACK():
                     return
